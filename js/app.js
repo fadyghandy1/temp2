34460bb953e1446150faed0cfd3c116e5cd6119c
Shroouk <shrouksaad5@gmail.com>
2022-12-18 15:35:41 +0200

    edit close icon in dark mode

--- js/app.js
+++ js/app.js
@@ -10,23 +10,31 @@ function Ck() {
 		document.body.style.backgroundImage = "url('../imgs/bg-simplify-section-desktop2.svg')";
 		document.body.style.color = "#fff";
 		//document.body.style.transform = "1s";
-		document.getElementById("logo-img").src = "../imgs/logo-dark.svg";
+		document.getElementById("logo-img").src = "./imgs/logo-dark.svg";
 
 		Array.from(document.getElementsByClassName("list-link")).forEach(element => {
 			element.classList.remove("list-link-light");
 			element.classList.add("list-link-dark");
 		});
 
+
+
 		Array.from(document.getElementsByClassName("card")).forEach(element => {
 			element.classList.remove("card-light");
 			element.classList.add("card-dark");
+		});
+
+		Array.from(document.getElementsByClassName("line")).forEach(element => {
+			element.classList.remove("line-light");
+			element.classList.add("line-dark");
+			
 		});
 
 	} else {
 		document.body.style.backgroundColor = "#fff";
 		document.body.style.backgroundImage = "url('../imgs/bg-simplify-section-desktop.svg')";
 		document.body.style.color = "#242d52";
-		document.getElementById("logo-img").src = "../imgs/logo1.svg";
+		document.getElementById("logo-img").src = "./imgs/logo1.svg";
 		Array.from(document.getElementsByClassName("list-link")).forEach(element => {
 			element.classList.add("list-link-light");
 			element.classList.remove("list-link-dark");
@@ -35,6 +43,11 @@ function Ck() {
 		Array.from(document.getElementsByClassName("card")).forEach(element => {
 			element.classList.add("card-light");
 			element.classList.remove("card-dark");
+		});
+		Array.from(document.getElementsByClassName("line")).forEach(element => {
+			element.classList.add("line-light");
+			element.classList.remove("line-dark");
+			
 		});
 	}
 
